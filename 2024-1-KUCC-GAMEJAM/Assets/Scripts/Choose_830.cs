c9cf39f16b4524c9055da441532cbb778102e715
jiwO0o <145910511+jiwO0o@users.noreply.github.com>
2024-05-11 04:58:08 +0900

    Add files via upload

---
+++ 2024-1-KUCC-GAMEJAM/Assets/Scripts/Choose_830.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//SceneManagement
+using UnityEngine.SceneManagement;
+
+public class Choose830 : MonoBehaviour
+{
+    public void StartButton()
+    {
+        SceneManager.LoadScene("Happy_ending");
+    }
+}
--- 2024-1-KUCC-GAMEJAM/Assets/Scripts/GameManager.cs
+++ 2024-1-KUCC-GAMEJAM/Assets/Scripts/GameManager.cs
@@ -1,41 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance;
-
-    private int playerScore = 0;
-    private int opponentScore = 100;
-
-    public TextMeshProUGUI playerCount;
-    public TextMeshProUGUI rivalCount;
-
-    void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+        
     }
 
-    public void AdjustScore(bool isPlayer, int amount)
+    // Update is called once per frame
+    void Update()
     {
-        if (isPlayer)
-        {
-            playerScore += amount;
-            playerCount.text = "내 당선가능성: " + playerScore.ToString();
-
-        }
-        else
-        {
-            opponentScore += amount;
-            rivalCount.text = "상대방 당선가능성: " + opponentScore.ToString();
-        }
+        
     }
 }
